40969f64ef94c66cb810838b3e5969f3b54fffc5
Matt Lim <mlimcollege@gmail.com>
2021-03-27 09:59:04 -0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hi</title>
+      </Head>
+      <div>
+        Wat
+      </div>
+    </div>
+  )
+}
