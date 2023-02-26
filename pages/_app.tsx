e22962dd1afb2e6867392d29514de21bdb864c34
Matt Lim <mlimcollege@gmail.com>
2023-02-26 09:00:10 -0800

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import "../styles/global.css";
 import "../styles/fonts/SpaceMono.css";
 import "../styles/fonts/WorkSans.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
